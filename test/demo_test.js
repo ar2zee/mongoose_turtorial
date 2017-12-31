2d1a7d69f4c7599c5d9e9552d3ea1bf760db0313
Artur Oganesyan <artur.og@yahoo.com>
2017-12-31 15:12:28 -0600

    Mocha first test

---
+++ test/demo_test.js
@@ -0,0 +1,10 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+describe('some test', function () {
+
+	it('addtwo numbers together', function(){
+		assert(2+3 === 5);
+		
+	})
+});
